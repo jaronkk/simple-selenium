db5d7391fe3475314309bb691f84701aed0c7d6f
Jaron Kennel <jaronkk@gmail.com>
2012-05-13 01:59:49 -0400

    Added a template_spec to use as a basis for new selenium test files

---
+++ spec/functional/template_spec.rb
@@ -0,0 +1,10 @@
+require "selenium_spec_helper"
+
+describe "whatever it is you're testing" do
+  it "should test something" do
+    # Open a website
+    # selenium.get("http://example.com")
+    # Test the page title
+    # selenium.title.should == "Your Page Title Here"
+  end
+end
